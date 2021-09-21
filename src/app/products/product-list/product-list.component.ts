0b5456874a8b36b2af96171a366a63406ccf4515
ldex <laurent.duveau+git@gmail.com>
2021-09-21 14:32:21 -0400

    Router

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
@@ -10,8 +12,30 @@ import { Product } from '../product.interface';
 export class ProductListComponent implements OnInit {
 
   title: string = 'Products';
-  products: Product[];
+  //products: Product[];
+  products$: Observable<Product[]>;
   selectedProduct: Product;
+  errorMessage: string;
+
+  // Pagination
+  pageSize = 5;
+  start = 0;
+  end = this.pageSize;
+  currentPage = 1;
+
+  previousPage() {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.currentPage--;
+    this.selectedProduct = null;
+  }
+
+  nextPage() {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.currentPage++;
+    this.selectedProduct = null;
+  }
 
   onSelect(product: Product): void {
     this.selectedProduct = product;
@@ -22,7 +46,24 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
+    this.products$ = this
+                      .productService
+                      .products$
+                      .pipe(
+                        catchError(
+                          error => {
+                            this.errorMessage = error;
+                            return EMPTY;
+                          }
+                        )
+                      );
+
+    // this
+    //   .productService
+    //   .products$
+    //   .subscribe(
+    //     results => this.products = results
+    //   );
   }
 
 }
